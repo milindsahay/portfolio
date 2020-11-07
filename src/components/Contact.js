a219c981c65dc40688ded5985671db6a7d41e37e
milindsahay <milind.sahay123@gmail.com>
2020-11-07 15:26:06 +0530

    Add Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Social from "./Social";
 
 const Contact = () => {
   return (
@@ -7,6 +8,25 @@ const Contact = () => {
         <span className="first-name">Contact </span>{" "}
         <span className="last-name">Me.</span>
       </div>
+      <div className="text">
+        Let's work together? Or possibly discuss some fictional endings to Game
+        of Thrones?
+      </div>
+      <div className="text">Drop me a message!</div>
+      <div className="row form">
+        <div className="col-8">
+          <textarea className="form-control" rows="10"></textarea>
+        </div>
+        <div className="col-4 button-container">
+          <button className="align-middle button btn btn-danger">
+            Send message!
+          </button>
+        </div>
+      </div>
+      <div className="text mt-5 mb-2">
+        We can connect over other platforms as well!
+      </div>
+      <Social />
     </div>
   );
 };
