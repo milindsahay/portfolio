4fc07c669885e9df4e442d522fa9dd6164e0305a
milindsahay <milind.sahay123@gmail.com>
2020-11-10 23:57:50 +0530

    About page enrichment

--- src/components/About.js
+++ src/components/About.js
@@ -11,21 +11,20 @@ const About = () => {
         Since childhood, Science and Technology was always what I looked up to.
         Due to my interest in programming, I immediately started dwelling deep
         into the fields of Computer Science. I am currently exploring the world
-        of machine learning, data science and web development. I have also used
-        C++, Python and Javascript with tools such as numpy, matplotlib, pandas,
-        scikit learn, ReactJS and Express throughout my academic projects. I
-        have won many Hackathons and competitions including Code Wars which
-        rigorously checked participant’s logic, reasoning and ability to code
-        within a stipulated time.I have also won Data Hack, North India's
-        biggest Data Science hackathon (in terms of prize money) and my team was
-        the first runner up in Infineon India Campus Hackathon in AI/ML track,
-        held in Bengaluru, India. Currently I'm working at Flipkart as a
-        Software Developer. As the world is becoming more technology-driven, I
-        realise that the possibilities of development and data in Computer
-        Science are limitless in each and every aspect of Computer Science and
-        still not yet explored completely. I take great pleasure to be part of
-        this field which is so innovative and promising. I love fictions and
-        Sci-fi films. I play guitar in my leisure time.
+        of web development, machine learning and data science. I have used C++,
+        Python, Java and Javascript with tools such as numpy, matplotlib,
+        pandas, scikit learn, ReactJS and Express throughout my academic
+        projects. I have won Data Hack, North India's biggest Data Science
+        hackathon (in terms of prize money) and my team was the first runner up
+        in Infineon India Campus Hackathon in AI/ML track, held in Bengaluru,
+        India. Occasionally, I write for the medium publication 'towards data
+        science' as well. Currently I'm working at Flipkart as a Software
+        Developer. As the world is becoming more technology-driven, I realise
+        that the possibilities of development and data in Computer Science are
+        limitless in each and every aspect of Computer Science and still not yet
+        explored completely. I take great pleasure to be part of this field
+        which is so innovative and promising. I love fictions and Sci-fi films.
+        I play guitar in my leisure time.
       </div>
     </div>
   );
