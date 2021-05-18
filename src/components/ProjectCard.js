19c4c143f459560b692f90dce3d00c608e0c0778
Milind Sahay <milind.sahay123@gmail.com>
2021-05-18 19:30:14 +0530

    project page data and css

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,16 +1,17 @@
 import React from "react";
-const ProjectCard = ({project}) =>{
-    const {name, content, image} = project
+
+const ProjectCard = ({project}) => {
+    const {name, content, image, link} = project
     return (
-        <div className="card mb-5">
-            <img className="card-img-top" src={image} alt="Card image cap" />
+        <a href={link} style={{textDecoration: 'none'}} className="card mb-5" target="_blank">
+            <img className="card-img-top" src={image} alt="Card image cap"/>
             <div className="card-body">
                 <p className="card-text">{content}</p>
             </div>
             <div className="card-footer text-center">
-                <h5 className=""><span className="card-symbol">&lt;</span> {name} <span className="card-symbol">/&gt;</span></h5>
+                <h5><span className="card-symbol">&lt;</span> {name} <span className="card-symbol">/&gt;</span></h5>
             </div>
-        </div>
+        </a>
     )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,33 +6,40 @@ const Projects = () => {
         {
             name: "Neural Style Transfer",
             content: "A deep learning model used to take two images—a content image and a style reference image (such as an artwork by a famous painter)—and blend them together so that the output converges to be a mixture of two.",
-            image: "https://florencefennel.co.in/image/cache/catalog/florencefennel/Machine--Learning-1000x1000.jpg"
+            image: "https://florencefennel.co.in/image/cache/catalog/florencefennel/Machine--Learning-1000x1000.jpg",
+            link: "https://github.com/milindsahay/NeuralStyleTransfer"
         },
         {
             name: "Credit Card Fraud Detection",
             content: "A deep learning model using encoder decoder architecture to detect frauds in credit card transactions.",
-            image: "https://florencefennel.co.in/image/cache/catalog/florencefennel/Machine--Learning-1000x1000.jpg"
+            image: "https://florencefennel.co.in/image/cache/catalog/florencefennel/Machine--Learning-1000x1000.jpg",
+            link: "https://github.com/milindsahay/CreditCardFraudDetection"
         },
         {
             name: "Recommender System",
             content: "A statistical machine learning model using user based collaborative filtering and exploratory data analysis to build a real time adaptive question recommendation system.",
-            image: "https://florencefennel.co.in/image/cache/catalog/florencefennel/Machine--Learning-1000x1000.jpg"
+            image: "https://florencefennel.co.in/image/cache/catalog/florencefennel/Machine--Learning-1000x1000.jpg",
+            link: "https://github.com/milindsahay/RecommenderSystem"
         },
         {
             name: "Think Tank",
             content: "A social blogging user authenticated application using react, redux and firebase.",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJnbjer-oBSRn1W5DLULk9eIOVm4ZY95cSPJG-OMNFN76KU41OhpJ7kjWQJQYVuqs_it0&usqp=CAU"
-        },
-        {
-            name: "Fox Game",
-            content: "A state machine game built with vanilla javascript, html and css.",
-            image: "https://brandslogo.net/wp-content/uploads/2015/07/javascript-logo-vector-download.jpg"
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJnbjer-oBSRn1W5DLULk9eIOVm4ZY95cSPJG-OMNFN76KU41OhpJ7kjWQJQYVuqs_it0&usqp=CAU",
+            link: "https://github.com/milindsahay/ThinkTank"
         },
         {
             name: "Adopt Me",
             content: "A pet adoption website using react",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJnbjer-oBSRn1W5DLULk9eIOVm4ZY95cSPJG-OMNFN76KU41OhpJ7kjWQJQYVuqs_it0&usqp=CAU"
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJnbjer-oBSRn1W5DLULk9eIOVm4ZY95cSPJG-OMNFN76KU41OhpJ7kjWQJQYVuqs_it0&usqp=CAU",
+            link: "https://github.com/milindsahay/AdoptMe"
         },
+        {
+            name: "Fox Game",
+            content: "A state machine game built with vanilla javascript, html and css.",
+            image: "https://brandslogo.net/wp-content/uploads/2015/07/javascript-logo-vector-download.jpg",
+            link: "https://github.com/milindsahay/FoxGame"
+        },
+
 
     ]
     return (
@@ -48,8 +55,8 @@ const Projects = () => {
                 }
             </div>
             <div className="row">
-                <div className="col-12">
-                    <button className="button btn btn-danger btn-block">See all</button>
+                <div className="col-12 text-center">
+                    <a href="https://github.com/milindsahay?tab=repositories" className="button btn btn-danger" target="_blank">Show all</a>
                 </div>
             </div>
         </div>
