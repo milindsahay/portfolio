6c06dd19a9dc7179e1c201a5f39e2d74eabab770
Milind Sahay <milind.sahay123@gmail.com>
2021-05-22 18:53:56 +0530

    refactor

--- src/components/logos/CareerLauncherLogo.js
+++ src/components/logos/CareerLauncherLogo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import careerlauncher from "./images/careerlauncher.jpg";
-
+//refactor into common logo icon
 const CareerLauncherLogo = () => {
   return <img src={careerlauncher} className="logo" />;
 };
